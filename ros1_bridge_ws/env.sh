94d5dac41e9be56ce9671597a0d70353ae0e1dd5
Allison Thackston <allison@lyonthackston.com>
2020-02-14 18:35:41 -0800

    added vscode workspace settings and dockerfile

---
+++ ros1_bridge_ws/env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/workspaces/deepracer_ws/ros1_bridge_ws/eloquent/lib:/opt/ros/eloquent/lib:/workspaces/deepracer_ws/ros1_bridge_ws/melodic/lib:/opt/ros/melodic/lib
+export AMENT_PREFIX_PATH=/workspaces/deepracer_ws/ros1_bridge_ws/eloquent:/opt/ros/eloquent
+export ROS_ETC_DIR=/opt/ros/melodic/etc/ros
+export COLCON_PREFIX_PATH=/workspaces/deepracer_ws/ros1_bridge_ws/eloquent:/opt/ros/eloquent
+export ROS_ROOT=/opt/ros/melodic/share/ros
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_VERSION=2
+export ROS_LOCALHOST_ONLY=0
+export ROS_PYTHON_VERSION=3
+export PYTHONPATH=/workspaces/deepracer_ws/ros1_bridge_ws/eloquent/lib/python3.6/site-packages:/opt/ros/eloquent/lib/python3.6/site-packages:/opt/ros/melodic/lib/python2.7/dist-packages
+export ROS_PACKAGE_PATH=/workspaces/deepracer_ws/ros1_bridge_ws/melodic/share:/opt/ros/melodic/share
+export ROSLISP_PACKAGE_DIRECTORIES=
+export PATH=/opt/ros/eloquent/bin:/opt/ros/melodic/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PKG_CONFIG_PATH=/workspaces/deepracer_ws/ros1_bridge_ws/melodic/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig
+export CMAKE_PREFIX_PATH=/workspaces/deepracer_ws/ros1_bridge_ws/eloquent:/opt/ros/eloquent
